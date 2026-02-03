e32bddb5ee46526cab66245b229e6ff05a9e7a35
root <root@ip-172-31-29-111.ec2.internal>
2026-02-03 02:27:50 +0000

    basic python code

---
+++ app.py
@@ -0,0 +1,10 @@
+# Variables
+name = "RaviKiranVarma"
+age = 25
+height = 1.75
+is_student = True
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height}")
+print(f"Student: {is_student}")
